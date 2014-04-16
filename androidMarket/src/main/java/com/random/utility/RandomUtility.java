42a7a28037d83220af25e7207219cd71e08d5afc
zfang <zfang442872966@gmail.com>
2014-04-16 18:45:23 +0800

    first commit

---
+++ androidMarket/src/main/java/com/random/utility/RandomUtility.java
@@ -0,0 +1,26 @@
+package com.random.utility;
+
+import java.util.Random;
+
+public class RandomUtility {
+
+	public final static String NUMBER = "0123456789";
+	public final static String LETTER = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	public final static String CHARACTER_LETTER = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	public final static String LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+	public final static String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	public static String getRandomString(String string, int length) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < length; i++) {
+			int number = new Random().nextInt(string.length());
+			sb.append(string.charAt(number));
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new Random().nextInt(10));
+		System.out.println(getRandomString(NUMBER, 10));
+	}
+}
