3e886eb056c457f0b97f5c7a550a13730508d470
zfang <zfang442872966@gmail.com>
2014-05-07 16:08:23 +0800

    test

---
+++ androidMarket/src/test/java/test/Test.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		byte[] bytes = { 42, 8, 90, 84, 69, 32, 85, 57, 51, 48 };
+		System.out.println(new String(bytes));
+	}
+}
