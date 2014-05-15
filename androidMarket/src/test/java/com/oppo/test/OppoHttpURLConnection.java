6c2c1385ce90b09d5ea07348032ab4bb009ff251
zfang <zfang442872966@gmail.com>
2014-05-15 14:45:58 +0800

    Protocl buffer

--- androidMarket/src/test/java/com/oppo/test/OppoHttpURLConnection.java
+++ androidMarket/src/test/java/com/oppo/test/OppoHttpURLConnection.java
@@ -7,12 +7,14 @@ import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
+
 /**
  * 
  * @author zfang
- *
+ * 
  */
 public class OppoHttpURLConnection {
 	/**
@@ -59,10 +61,13 @@ public class OppoHttpURLConnection {
 		return b;
 	}
 
-	public static void main(String[] args) throws IOException {
-		byte abyte0[] = { 8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 16, 15, 24, 20, 32, 0, 42, 8, 90, 84, 69, 32, 85, 57, 51, 48, 56, 7, 80, 1, 120, 1 };
-		System.out.println(new String(abyte0));
-		System.out.println("-----------------------------------------------------------------");
+	/**
+	 * HttpURLConnection
+	 * 
+	 * @throws IOException
+	 */
+	public static byte[] httpURLConnection(String url, byte[] abyte0) throws IOException {
+		byte[] result;
 		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.4.69", 8888));
 
 		HttpURLConnection connection = (HttpURLConnection) new URL("http://i3.store.nearme.com.cn/client/get_hot_app.pb").openConnection(proxy);
@@ -92,9 +97,10 @@ public class OppoHttpURLConnection {
 		while ((number = connection.getInputStream().read()) != -1) {
 			baos.write(number);
 		}
-		System.out.println(new String(decompressGzip(baos.toByteArray())));
+		result = baos.toByteArray();
+		// System.out.println(new String(decompressGzip(baos.toByteArray())));
 		baos.close();
-
+		connection.disconnect();
 		// BufferedReader reader = new BufferedReader(new
 		// InputStreamReader(connection.getInputStream()));
 		// String lines;
@@ -102,6 +108,50 @@ public class OppoHttpURLConnection {
 		// System.out.println(lines);
 		// }
 		// reader.close();
-		connection.disconnect();
+		return result;
+	}
+
+	static int makeTag(int i, int j) {
+		return j | i << 3;
+	}
+
+	public static void main(String[] args) throws IOException {
+		// [8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 16, 15, 24, 20, 32, 0, 42,
+		// 8, 90, 84, 69, 32, 85, 57, 51, 48, 56, 7, 80, 1, 120, 1]
+
+		// [8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 16, 15, 24, 20, 32, 20,
+		// 42, 8, 90, 84, 69, 32, 85, 57, 51, 48, 56, 7, 80, 1, 120, 1]
+
+		// [8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 16, 15, 24, 20, 32, 40,
+		// 42, 8, 90, 84, 69, 32, 85, 57, 51, 48, 56, 7, 80, 1, 120, 1]
+
+		// [8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 16, 15, 24, 20, 32, 60,
+		// 42, 8, 90, 84, 69, 32, 85, 57, 51, 48, 56, 7, 80, 1, 120, 1]
+
+		// [8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 16, 15, 24, 20, 32, 80,
+		// 42, 8, 90, 84, 69, 32, 85, 57, 51, 48, 64, 3, 80, 1, 120, 1]
+
+		// [8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 16, 15, 24, 20, 32, 100,
+		// 42, 8, 90, 84, 69, 32, 85, 57, 51, 48, 64, 3, 80, 1, 120, 1]
+
+		// [8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 16, 15, 24, 20, 32, 120,
+		// 42, 8, 90, 84, 69, 32, 85, 57, 51, 48, 64, 3, 80, 1, 120, 1]
+
+		// [8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 16, 15, 24, 20, 32, -116,
+		// 1, 42, 8, 90, 84, 69, 32, 85, 57, 51, 48, 64, 3, 80, 1, 120, 1]
+
+		// [8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 16, 15, 24, 20, 32, -96,
+		// 1, 42, 8, 90, 84, 69, 32, 85, 57, 51, 48, 64, 3, 80, 1, 120, 1]
+
+		// [8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 16, 15, 24, 20, 32, -76,
+		// 1, 42, 8, 90, 84, 69, 32, 85, 57, 51, 48, 64, 3, 80, 1, 120, 1]
+
+		byte[] abyte0 = { 8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 16, 15, 24, 20, 32, 0, 42, 8, 90, 84, 69, 32, 85, 57, 51, 48, 56, 7, 80, 1, 120, 1 };
+		System.out.println(new String(abyte0));
+		System.out.println("------------------------------------------------------------------------------");
+		byte[] result = httpURLConnection("http://i3.store.nearme.com.cn/client/get_hot_app.pb", abyte0);
+		System.out.println(new String(decompressGzip(result)));
+		System.out.println("------------------------------------------------------------------------------");
+		System.out.println(Arrays.toString(decompressGzip(result)));
 	}
 }
