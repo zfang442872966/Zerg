d2d5c3fdbe45dd32f0c283f549cbffe77b88ebbc
zfang <zfang442872966@gmail.com>
2014-05-21 17:57:28 +0800

    Increase the utility class

---
+++ androidMarket/src/main/java/com/market/service/implement/Bytes2HexString.java
@@ -0,0 +1,45 @@
+package com.market.service.implement;
+
+/**
+ * byte[]和16进制互相转换
+ * @author zfang
+ *
+ */
+public class Bytes2HexString {
+	private final static byte[] hex = "0123456789ABCDEF".getBytes();
+
+	private static int parse(char c) {
+		if (c >= 'a')
+			return (c - 'a' + 10) & 0x0f;
+		if (c >= 'A')
+			return (c - 'A' + 10) & 0x0f;
+		return (c - '0') & 0x0f;
+	}
+
+	/**
+	 * 从字节数组到十六进制字符串转换
+	 * 
+	 * @param b
+	 * @return
+	 */
+	public static String bytes2HexString(byte[] b) {
+		byte[] buff = new byte[2 * b.length];
+		for (int i = 0; i < b.length; i++) {
+			buff[2 * i] = hex[(b[i] >> 4) & 0x0f];
+			buff[2 * i + 1] = hex[b[i] & 0x0f];
+		}
+		return new String(buff);
+	}
+
+	// 从十六进制字符串到字节数组转换
+	public static byte[] hexString2Bytes(String hexstr) {
+		byte[] b = new byte[hexstr.length() / 2];
+		int j = 0;
+		for (int i = 0; i < b.length; i++) {
+			char c0 = hexstr.charAt(j++);
+			char c1 = hexstr.charAt(j++);
+			b[i] = (byte) ((parse(c0) << 4) | parse(c1));
+		}
+		return b;
+	}
+}
