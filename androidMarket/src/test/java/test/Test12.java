00e0fe990049210ad575a50246c40fb9078fad1f
zfang <zfang442872966@gmail.com>
2014-05-29 10:17:19 +0800

    test

--- androidMarket/src/test/java/test/Test12.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-
-public class Test12 {
-	public static void main(String[] args) {
-	}
-}
--- androidMarket/src/test/java/test/Test5.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class Test5 {
-
-	public static void main(String[] args) {
-		Object r = (Runnable) () -> System.out.println("hello,lambda");
-	}
-
-}
